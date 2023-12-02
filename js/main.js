a467bf9cfa904c9f445b02a93380c89710158e02
And-code <andrewgr.mob@gmail.com>
2023-12-02 23:25:59 +0200

    finished

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const image = document.createElement('img');
 const imgRef = Math.floor(Math.random() * 9) + 1;
-
 image.setAttribute('src', `./images/${imgRef}.jpg`);
 document.querySelector('#image').appendChild(image);
